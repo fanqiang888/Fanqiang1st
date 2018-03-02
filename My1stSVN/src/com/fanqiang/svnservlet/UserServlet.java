99eb421fe1650d476b55bc376040f8217d3fb8bd
fanqiang888 <36975112+fanqiang888@users.noreply.github.com>
2018-03-02 11:06:01 +0800

    Update UserServlet.java

--- My1stSVN/src/com/fanqiang/svnservlet/UserServlet.java
+++ My1stSVN/src/com/fanqiang/svnservlet/UserServlet.java
@@ -34,7 +34,7 @@ public class UserServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub  在github上修改的
 		doGet(request, response);
 	}
 
