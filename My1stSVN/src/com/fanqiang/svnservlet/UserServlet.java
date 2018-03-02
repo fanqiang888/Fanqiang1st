0558a8651faff0288afa97f1b28529bdcf959a52
fanqiang888 <fqaxcgnzhlj.com.cn@qq.com>
2018-03-02 11:01:00 +0800

    修改Servlet

--- My1stSVN/src/com/fanqiang/svnservlet/UserServlet.java
+++ My1stSVN/src/com/fanqiang/svnservlet/UserServlet.java
@@ -26,7 +26,7 @@ public class UserServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 修改了源代码
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
